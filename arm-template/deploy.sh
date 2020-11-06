8c7a552994a47800b47729bdcd4c44e901d46f44
anildwarepo <anildwa@MININT-VUAA1KN.redmond.corp.microsoft.com>
2020-11-06 09:49:10 -0800

    arm template

--- arm-template/deploy.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-az group create -g databricks -l westus
-#az deployment group create -g databricks  --template-file databrick-scc-deploy.json
-az deployment group create -g databricks  --template-file databrick-scc-deploy.json --parameters deploy.parameters.json
-
